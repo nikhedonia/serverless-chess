919183f45aebfa4fa6776489a6e48c5dd61d941b
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-09-04 00:54:18 +0100

    feat: adds history

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
 import React, { Component } from 'react';
 import Board from "react-chess";
-import logo from './logo.svg';
 
-const engine = require("chess.js");
-
-
-
+import engine from "chess.js";
 
 class App extends Component {
-  constructor(){
-    super();
 
-    const hash = window.location.hash.slice(1);
-    const fen = (hash!='')? window.atob(hash) : undefined;
-    
-    this.chess = new engine(fen);
-    this.fields = () => {
-      return this.chess.SQUARES
-        .map( k => [k, this.chess.get(k)])
-        .filter( ([k, v]) => v)
-        .map( ([k, {type, color}]) => [k, `${(color=='w')? type.toUpperCase() : type}`])
-        //.reduce( (a, b) => ({...a, ...b}), {})
+  movesFromHash(hash) {
+    return window.atob(hash.slice(1))
+      .split('|')
+      .filter(x=>x!='');
+  }
+
+  getEngine(moves) {
+    const chess = new engine();
+    for (const move of moves) {
+      chess.move(move);
     }
+    return chess;
+  }
 
-    this.pieces = () => this.fields().map( ([k, v]) => `${v}@${k}`);
-    
+  fields(chess) {
+    return chess.SQUARES
+      .map( k => [k, chess.get(k)])
+      .filter( ([k, v]) => v)
+      .map( ([k, {type, color}]) => [k, `${(color=='w')? type.toUpperCase() : type}`])
+  }
+
+  pieces(chess) {
+    return this.fields(chess).map( ([k, v]) => `${v}@${k}`);
+  }
+
+  history() {
+    return this.state.history.slice(0, this.state.history.length-this.state.undo);
+  }
+
+
+  constructor(props) {
+    super(props);
+
+    const chess = this.getEngine(
+      this.movesFromHash(props.state)
+    );
+
     this.state = {
-      pieces: this.pieces(),
-      player: 0
+      history: chess.history(),
+      undo: 0 
     };
   }
 
-  shouldComponentUpdate(){ console.log('blub'); return true; }
+  shouldComponentUpdate() {
+    return true; 
+  }
 
-  renderState() {
-    const check = this.chess.in_check(); 
-    const gameOver = this.chess.game_over(); 
-    const turn = this.chess.turn() =="w"? "white's turn" : "black's turn";
+  renderState(chess) {
+    const check = chess.in_check(); 
+    const gameOver = chess.game_over(); 
+    const turn = chess.turn() =="w"? "white's turn" : "black's turn";
 
     if (check) {
       return <div>
@@ -52,6 +71,8 @@ class App extends Component {
   }
 
   render() {
+    const chess = this.getEngine(this.history());
+
     return (
       <div className="App" style={{
         display: "flex", 
@@ -60,14 +81,13 @@ class App extends Component {
         alignItems: "center",
       }}>
         <div style={{textAlign:"center"}}> serverless chess </div>
-        {this.renderState()}
+        {this.renderState(chess)}
         <div style={{
           display:"flex",
           justifyContent: "stretch",
           alignItems: "stretch",
           flexDirection: "row",
           flex: 1,
-          background:"red",
           minWidth: "300px",
           minHeight:"300px",
           height: "90vh",
@@ -75,19 +95,47 @@ class App extends Component {
           maxHeight: "90vh",
           maxWidth: "90vh"
         }}>
+
         <Board 
-          pieces={this.state.pieces}
+          pieces={this.pieces(chess)}
           onMovePiece={ (piece, from, to) => {
-            const m = this.chess.move(`${piece.name}${from}-${to}`,  {sloppy: true});
-            if (!m) this.setState({pieces:[]});
-            setTimeout(() => this.setState({pieces:this.pieces()}));
-            window.location.hash = window.btoa(this.chess.fen());
+            const chess = this.getEngine(this.history());
+            const move = `${piece.name}${from}-${to}`;
+            const m = chess.move(move,  {sloppy: true});
+
+            if(!m) this.setState({history:[]});
+
+            setTimeout(() => this.setState({
+              history: chess.history(),
+              undo: 0
+            }));
+
+            window.location.hash = window.btoa(this.history(chess).join('|'));
           }}
 
           onDragStart={ (piece, from) => {
-            return (piece.name.toUpperCase() == piece.name) == (this.chess.turn() == 'w');
-          }}
-        />
+            return (piece.name.toUpperCase() == piece.name) == (chess.turn() == 'w');
+          }}/>
+
+        <div style={{height:"100%"}}>
+          <h2> History </h2> 
+          <ol style={{overflow:"auto", height:"80%"}}>
+            {this.state.history.map((move, i) =>
+            <li key={i} 
+              style={ 
+                (this.state.history.length-i-1 == this.state.undo)
+                ? {color:'green'} 
+                : {}
+              }
+              onMouseEnter={()=>{
+                this.setState({undo: this.state.history.length-i-1})
+                window.location.hash = window.btoa(this.history(chess).join('|'));
+              }}>
+                {move} 
+              </li>
+            )}
+          </ol>
+        </div>
       </div>
     </div>
     );
